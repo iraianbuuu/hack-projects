afde66563da1e9d01aae40b9269a6d047ef3781b
Arpit <arpitsengar99@gmail.com>
2024-10-25 11:42:09 +0530

    fix: Type error: File '/vercel/path0/app/about/page.tsx' is not a module.

---
+++ app/about/page.tsx
@@ -0,0 +1,10 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'HackProjects | Contact Us',
+  description: 'Contact Us.'
+};
+
+export default function Page() {
+  return <h1>Contact Us Page goes here</h1>;
+}
