d751965f2a28a9dbee415d4c96340b33993bd82d
Arpit <arpitsengar99@gmail.com>
2025-10-07 06:17:41 +0530

    add headers

--- server/app.py
+++ server/app.py
@@ -20,7 +20,20 @@ def fetch_github_page(**kwargs) -> str:
     try:
         params = '&'.join([f'{k}={v}' for k, v in kwargs.items()])
         url = 'https://github.com/topics/hacktoberfest?{}'.format(params)
-        response = requests.get(url)
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+            'Accept-Language': 'en-US,en;q=0.9',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Connection': 'keep-alive',
+            'Upgrade-Insecure-Requests': '1',
+            'Sec-Fetch-Dest': 'document',
+            'Sec-Fetch-Mode': 'navigate',
+            'Sec-Fetch-Site': 'none',
+            'Sec-Fetch-User': '?1',
+            'Cache-Control': 'max-age=0',
+        }
+        response = requests.get(url, headers)
         response.raise_for_status()
         
         return response.text
